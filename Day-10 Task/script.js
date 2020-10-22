af5d1ff577148c4c573893b83160cfc37d207be5
Ibrat Nawaz <ibratrocker99@gmail.com>
2020-10-22 14:57:51 +0530

    minor change in task 10

--- Day-10 Task/script.js
+++ Day-10 Task/script.js
@@ -2,14 +2,14 @@ let row = document.querySelector('.row');
 let result = document.createElement('div');
 result.classList.add('result');
 result.innerHTML = `
-    <div class="sec-1 num" id="inputs">0 =</div>
+    <div class="sec-1 num" id="inputs">0</div>
     <div class="sec-1 res" id="output">0</div>
 `;
 let keys = document.createElement('div');
 keys.classList.add('keys');
 keys.innerHTML = `
     <button class="bold" onclick="calculate('%')" id="btn-4">%</button>
-    <button class="bold" onclick="clearFun('cls')" id="btn-8">CE</button>
+    <button class="bold" onclick="clearFun('cls')" id="btn-8">ESC</button>
     <button class="bold" onclick="clearFun('all')" id="btn-7">C</button>
     <button class="bold" onclick="calculate('BS')" id="btn-9">&#9224;</button>
     <button onclick='calculate(9)'>9</button>
@@ -58,12 +58,12 @@ calculate = (key) => {
 clearFun = (event) => {
     if (event == 'cls') {
         exp = '';
-        document.getElementById('inputs').innerText = '0 =';
+        document.getElementById('inputs').innerText = '0';
         document.getElementById('output').innerText = '0';
     }
 }
 
-window.onkeydown = (keyPress) => {
+document.body.onkeydown = (keyPress) => {
 
     if (keyPress.keyCode == 13){
         calculate('=');
@@ -72,12 +72,16 @@ window.onkeydown = (keyPress) => {
     else if(keyPress.keyCode == 8){
         calculate('BS');
     }
+    
+    else if(keyPress.keyCode == 27){
+        clearFun('cls');
+    }
 
     else if ((keyPress.key >= 0 && keyPress.key <= 9) ||
-        keyPress.key <= '%' || keyPress.key <= '/' ||
-        keyPress.key <= '*' || keyPress.key <= '-' ||
-        keyPress.key <= '+') {
-
+        keyPress.key == '%' || keyPress.key == '/' ||
+        keyPress.key == '*' || keyPress.key == '-' ||
+        keyPress.key == '+') {
+            
         calculate(keyPress.key);
     }
 
