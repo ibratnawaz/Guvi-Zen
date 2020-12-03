d838f60565509083db349c415cc85b256b61c545
Ibrat Nawaz <51021308+nawazibrat@users.noreply.github.com>
2020-12-03 20:42:50 +0530

    Update script.js

--- Day-30-Task/script.js
+++ Day-30-Task/script.js
@@ -1,15 +1,30 @@
 getUser(1);
 getUser(2);
+// https://nodejs-zen.herokuapp.com
 async function getUser(role) {
     try {
         let api;
         let table;
+        let target = '';
+        let funName;
+        let ancName;
+        let viewName;
+        let viewFunc;
         if (role == 1) {
             api = await fetch('https://nodejs-zen.herokuapp.com/students');
             table = document.getElementById('student-table');
+            ancName = 'Remove Mentor';
+            funName = 'removeAssignedMentor';
+            viewFunc = 'viewMentor';
+            viewName = 'View Mentor';
         } else if (role == 2) {
             api = await fetch('https://nodejs-zen.herokuapp.com/mentors');
             table = document.getElementById('mentor-table');
+            target = '#modal-assign';
+            ancName = 'Assign New';
+            funName = 'showStudents';
+            viewFunc = 'getAssignedStudent';
+            viewName = 'View Students';
         }
         let res = await api.json();
         if (res.items) {
@@ -26,10 +41,13 @@ async function getUser(role) {
                 td4.innerHTML = ele.phone;
                 let td5 = createMyTag('td');
                 td5.innerHTML = `
-                <button class="btn btn-link text-warning" data-toggle="modal" 
-                data-target="#modal" onclick="editUser('${role}','${ele['_id']}','${ele.name}','${ele.email}','${ele.phone}')">Edit
-                </button>
-                <button class="btn btn-link text-danger" onclick="deleteUser('${role}','${ele["_id"]}')">Delete</button>`;
+                    <button class="btn btn-link text-warning" data-toggle="modal" 
+                    data-target="#modal" onclick="editUser('${role}','${ele['_id']}','${ele.name}','${ele.email}','${ele.phone}')">Edit
+                    </button>
+                    <button class="btn btn-link text-danger" onclick="deleteUser('${role}','${ele["_id"]}')">Delete</button>
+                    <button class="btn btn-link text-primary" data-toggle="modal" onclick="${funName}('${ele["_id"]}')"
+                    data-target="${target}">${ancName}</button>
+                    <button class="btn btn-link text-success" onclick="${viewFunc}('${ele["_id"]}')">${viewName}</button>`;
                 tr.append(td1, td2, td3, td4, td5);
                 table.appendChild(tr);
             });
@@ -185,5 +203,119 @@ async function deleteUser(role, userId) {
         }
     } catch (error) {
         console.log(error)
+    }
+}
+
+
+async function assignStudent() {
+    try {
+        document.getElementById('btn-assign').disabled = true;
+        let mentorId = document.getElementById('mentorId').value;
+        let students = [];
+        let options = document.getElementById('students').options;
+        for (let option of options) {
+            if (option.selected) {
+                students.push(option.value);
+            }
+        }
+        let data = {
+            mentorId,
+            students,
+        }
+        let api = await fetch('https://nodejs-zen.herokuapp.com/assign/students', {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json"
+            }
+        });
+        let res = await api.json();
+        $('#modal-assign').modal('hide');
+        document.getElementById('btn-assign').disabled = false;
+        alertMessage(res.message, 'success-msg');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function showStudents(mentorID) {
+    try {
+        let api = await fetch('https://nodejs-zen.herokuapp.com/assign/view/students');
+        let res = await api.json();
+        let select = document.getElementById('students');
+        if (res.items) {
+            document.getElementById('mentorId').value = `${mentorID}`;
+            select.innerHTML = ``;
+            res.data.map(obj => {
+                let option = createMyTag('option');
+                option.setAttribute('value', `${obj['_id']}`);
+                option.innerHTML = `${obj.name}`;
+                select.appendChild(option);
+            })
+        } else {
+            select.innerHTML = `<option>No student available</option>`;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getAssignedStudent(mentorId) {
+    try {
+        $('#mentor-students').modal('show');
+        document.getElementById('assignedModalLabel').innerHTML='Assigned Students';
+        let api = await fetch(`https://nodejs-zen.herokuapp.com/assign/mentor/students/${mentorId}`);
+        let res = await api.json();
+        let list = document.getElementById('students-list');
+        if (res.status == 'success') {
+            list.innerHTML = '';
+            res.data[0].results.map(obj => {
+                let li = createMyTag('li', 'list-group-item');
+                li.innerHTML = `${obj.name}`;
+                list.appendChild(li);
+            })
+        } else {
+            list.innerHTML = '';
+            let li = createMyTag('li', 'list-group-item');
+            li.innerHTML = `${res.message}`;
+            list.appendChild(li);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function removeAssignedMentor(mentorId) {
+    try {
+        let decision = confirm('You sure you want to remove your mentor');
+        if (decision) {
+            let api = await fetch(`https://nodejs-zen.herokuapp.com/assign/mentor/remove/${mentorId}`, {
+                method: "DELETE",
+            });
+            let res = await api.json();
+            alertMessage(res.message, 'danger');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function viewMentor(id){
+    try {
+        $('#mentor-students').modal('show');
+        document.getElementById('assignedModalLabel').innerHTML='Assigned Mentor';
+        let api = await fetch(`https://nodejs-zen.herokuapp.com/assign/mentor/${id}`);
+        let res = await api.json();
+        let list = document.getElementById('students-list');
+        list.innerHTML = '';
+        let li = createMyTag('li', 'list-group-item');
+        if (res.status=='success') {
+            li.innerHTML = `${res.data[0].results[0].name}`;
+        } else {
+            li.innerHTML = `${res.message}`;
+        }
+        list.appendChild(li);
+    } catch (error) {
+        
     }
 }
