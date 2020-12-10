ea7101ed616d657b8469b7b212ce77f3616f9af5
Ibrat Nawaz <ibratrocker99@gmail.com>
2020-12-10 20:18:45 +0530

    dashboard completed

--- Day-33-Task/assets/js/dashboard.js
+++ Day-33-Task/assets/js/dashboard.js
@@ -9,10 +9,35 @@ function logout() {
 }
 
 async function createShortUrl() {
+    document.getElementById('btn-url').disabled = true;
     let data = {
-        longUrl: document.getElementById('url').value
+        long_url: document.getElementById('url').value
     }
-    console.log(data);
+    let apiShortUrl = await fetch('http://localhost:3000/short-url', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            "Authorization": localStorage.getItem('_token'),
+            "Content-type": "application/json"
+        }
+    });
+
+    let response = await apiShortUrl.json();
+    console.log(response);
+    showAlert(response.message, 'success');
+    document.getElementById('btn-url').disabled = false;
+    getData();
+}
+
+function showAlert(message, status) {
+    let alertBox = document.getElementById('alert-message');
+    alertBox.removeAttribute('class');
+    alertBox.style.display = 'block';
+    alertBox.setAttribute('class', `alert alert-${status}`);
+    alertBox.innerHTML = message;
+    setTimeout(() => {
+        alertBox.style.display = 'none';
+    }, 2000);
 }
 
 async function getData() {
@@ -24,9 +49,30 @@ async function getData() {
             }
         });
         let response = await (await api).json();
-        console.log(response);
+        let table = document.getElementById('data-table');
+        if (response.data[0].results.length) {
+            table.innerHTML = '';
+            response.data[0].results.forEach((obj, index) => {
+                let tr = document.createElement('tr');
+                tr.innerHTML = `
+                    <th scope="row">${index+1}</th>
+                    <td title="${obj.long_url}">${obj.long_url.substring(0,49)}...</td>
+                    <td>http://localhost:3000/${obj.short_url_code}</td>
+                    <td><a href="http://localhost:3000/${obj.short_url_code}" target="_blank"><i class="fa fa-link"></i></a></td>
+                `;
+                table.appendChild(tr);
+            });
+        } else {
+            table.innerHTML = '';
+            let tr = document.createElement('tr');
+            tr.innerHTML = `
+                    <td class="text-center" colspan="4">No data</td>
+                `;
+            table.appendChild(tr);
+        }
     } catch (error) {
         console.log(error);
     }
 }
+
 getData()
